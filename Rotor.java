4115d68916e896b6e378cd29c5605d7f7ef0b514
lpizzolato8 <146384522+lpizzolato8@users.noreply.github.com>
2024-09-25 16:28:10 -0400

    index and char methods

--- Rotor.java
+++ Rotor.java
@@ -1,28 +1,38 @@
 public class Rotor {
 
-    
     private String rotorValues;
     private char startChar;
-        
-    public Rotor(String v, char c){
+
+    public Rotor(String v, char c) {
         this.rotorValues = new String(v);
         this.startChar = c;
-        
-        while(!this.rotate());
-            
-    }
-    
-    public boolean rotate(){
-        //TODO
-               
-    }
-    
 
-    public int indexOf(char c){
-        //TODO
+
+        char node;
+
     }
 
-    public char charAt(int idx){
-        //TODO
+    public boolean rotate() {
+        // TODO
+
+        char node = rotorValues.charAt(0);
+
+
+
+        if (node == startChar) {
+            return true;
+        }
+        return false;
+    }
+
+    public int indexOf(char c) {
+        // TODO
+
+       return rotorValues.indexOf(c);
+    }
+
+    public char charAt(int idx) {
+        // TODO
+        return rotorValues.charAt(idx);
     }
 }
