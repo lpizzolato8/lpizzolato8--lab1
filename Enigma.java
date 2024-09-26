0c16b5df8729f4369dc18ae2d8be9f1cd23a9c76
lpizzolato8 <lpizzolato8@gmail.com>
2024-09-26 16:11:26 -0400

    Update Enigma.java
    Work on enigma done

--- Enigma.java
+++ Enigma.java
@@ -1,10 +1,12 @@
 public class Enigma{
 
-    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
+    private String rotorInit[] = {
+        "#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
         "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
+        "#TGOWHLIFMCSZYRVXQABUPEJKND"
+    };
 
 
     private Rotor rotors[];
@@ -27,6 +29,35 @@ public class Enigma{
     
     public String encrypt(String message){
         //TODO
+
+        // find the character on the outermost rotor and get index. -- rotor3.indexof(a)
+        // find the chartacter on the innermost rotor using the index -- rotor1.charAt(rotor3.indexof(a))
+        // find the character on the middle rotor and get the index -- rotor2.indexof(^^)d
+        // find the character on the outermost rotor using index -- rotor3.charAt(^^^)
+        // Rotate rotor3
+        // if same value then middle rotates -- if rotate = true, rotat middle
+
+        if(rotors[0].rotate()){
+            rotors[2].rotate();
+        }
+        
+        for(int i = 0; i < message.length(); i++){
+            rotors[3].indexOf(message.charAt(i));
+        rotors[1].charAt();
+        rotors[2].indexOf();
+        rotors[3].charAt();
+
+        }
+
+        rotors[3].indexOf();
+        rotors[1].charAt();
+        rotors[2].indexOf();
+        rotors[3].charAt();
+
+        rotors[3].rotate();
+
+
+
 
     }
     private void rotate(){
