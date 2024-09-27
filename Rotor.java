465ade1f2743089f680a8418d3639d056eb9a9e3
lpizzolato8 <lpizzolato8@gmail.com>
2024-09-27 11:25:42 -0400

    Attempt to fix out of bounds error

--- Rotor.java
+++ Rotor.java
@@ -2,33 +2,38 @@ public class Rotor {
 
     private String rotorValues;
     private char startChar;
-
-    public Rotor(String v, char c) {        
+    public Rotor(String v, char c) {
         this.rotorValues = new String(v);
         this.startChar = c;
+        String copy = "";
 
-        String temp = rotorValues.substring(c, c);
-        String temp1 = rotorValues.substring(0, startChar-1);
-        this.rotorValues = temp + temp1;
-    }   
+        for (int i = 0; i < rotorValues.length(); i++) {
+            if (rotorValues.charAt(i) != this.startChar) {
+                copy = copy + rotorValues.charAt(i);
+            }
+        }
+        rotorValues = this.startChar + copy;
+    }
 
     public boolean rotate() {
         // TODO
-        char node = rotorValues.charAt(0);
-        String temp = rotorValues.substring(0, rotorValues.length() - 1);
-        String temp1 = rotorValues.substring(rotorValues.length()-1, rotorValues.length()-1);
-        rotorValues = temp1 + temp;
-        
+        char node = rotorValues.charAt(rotorValues.length()-1);
         if (node == startChar) {
+            String temp = rotorValues.substring(0, rotorValues.length() - 2);
+            String temp1 = rotorValues.substring(rotorValues.length() - 1, rotorValues.length() - 1);
+            rotorValues = temp1 + temp;
             return true;
         }
+        String temp3 = rotorValues.substring(0, rotorValues.length() - 2);
+        String temp4 = rotorValues.substring(rotorValues.length() - 1, rotorValues.length() - 1);
+        rotorValues = temp4 + temp3;
         return false;
     }
 
     public int indexOf(char c) {
         // TODO
 
-       return rotorValues.indexOf(c);
+        return rotorValues.indexOf(c);
     }
 
     public char charAt(int idx) {
