4514683aba8d5ecfc80b903d0cc7392c80a76019
lpizzolato8 <lpizzolato8@gmail.com>
2024-09-27 12:55:01 -0400

    Fixed Rotor.java by changing the constructor to rotate the rotors/string

--- Enigma.java
+++ Enigma.java
@@ -1,82 +1,73 @@
-public class Enigma{
+public class Enigma {
 
     private String rotorInit[] = {
-        "#GNUAHOVBIPWCJQXDKRYELSZFMT",
-        "#EJOTYCHMRWAFKPUZDINSXBGLQV",
-        "#BDFHJLNPRTVXZACEGIKMOQSUWY",
-        "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"
+            "#GNUAHOVBIPWCJQXDKRYELSZFMT",
+            "#EJOTYCHMRWAFKPUZDINSXBGLQV",
+            "#BDFHJLNPRTVXZACEGIKMOQSUWY",
+            "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
+            "#TGOWHLIFMCSZYRVXQABUPEJKND"
     };
 
-
     private Rotor rotors[];
-        
-    public Enigma(int id1, int id2, int id3, String start){
+
+    public Enigma(int id1, int id2, int id3, String start) {
 
         rotors = new Rotor[3];
-        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
-        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
-        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
-        
+        rotors[0] = new Rotor(rotorInit[id1 - 1], start.charAt(0));
+        rotors[1] = new Rotor(rotorInit[id2 - 1], start.charAt(1));
+        rotors[2] = new Rotor(rotorInit[id3 - 1], start.charAt(2));
+
     }
 
-
-    public String decrypt(String message){        
-        //TODO
+    public String decrypt(String message) {
+        // TODO
 
         String output = "";
 
-        if(rotors[0].rotate()){
-            rotors[2].rotate();
-        }
-
-        for(int i = 0; i < message.length() ; i++){
-        int rahhh = rotors[2].indexOf(message.charAt(i));
-        char rahh = rotors[1].charAt(rahhh); 
-        int rah = rotors[0].indexOf(rahh);
-        char ra = rotors[2].charAt(rah);
-        output = output + ra;
-        rotors[2].rotate();
+        for (int i = 0; i < message.length(); i++) {
+            int indexOfCharInnerRotor = rotors[2].indexOf(message.charAt(i));
+            char charAlignedOuterRotor = rotors[1].charAt(indexOfCharInnerRotor);
+            int indexOfCharMiddleRotor = rotors[2].indexOf(charAlignedOuterRotor);
+            char finalChar = rotors[0].charAt(indexOfCharMiddleRotor);
+            output = output + finalChar;
+            rotate();
         }
 
         return output;
     }
 
-
-    
-    public String encrypt(String message){
-        //TODO
+    public String encrypt(String message) {
+        // TODO
 
         // find the character on the outermost rotor and get index. -- rotor3.indexof(a)
-        // find the chartacter on the innermost rotor using the index -- rotor1.charAt(rotor3.indexof(a))
-        // find the character on the middle rotor and get the index -- rotor2.indexof(^^)d
+        // find the chartacter on the innermost rotor using the index --
+        // rotor1.charAt(rotor3.indexof(a))
+        // find the character on the middle rotor and get the index --
+        // rotor2.indexof(^^)d
         // find the character on the outermost rotor using index -- rotor3.charAt(^^^)
         // Rotate rotor
         // if same value then middle rotates -- if rotate = true, rotat middle
 
-        if(rotors[0].rotate()){
-            rotors[1].rotate();
-        }
         String cypher = "";
 
-        for(int i = 0; i < message.length(); i++){
-        int rahhh = rotors[2].indexOf(message.charAt(i));
-        char rahh = rotors[0].charAt(rahhh); 
-        int rah = rotors[1].indexOf(rahh);
-        char ra = rotors[2].charAt(rah);
-        cypher = cypher + ra;
-        rotors[2].rotate();
+        for (int i = 0; i < message.length(); i++) {
+            int indexOfCharInnerRotor = rotors[0].indexOf(message.charAt(i));
+            char charAlignedOuterRotor = rotors[2].charAt(indexOfCharInnerRotor);
+            int indexOfCharMiddleRotor = rotors[1].indexOf(charAlignedOuterRotor);
+            char finalChar = rotors[2].charAt(indexOfCharMiddleRotor);
+            cypher = cypher + finalChar;
+            rotate();
         }
-        
+
         return cypher;
 
-
     }
-    private void rotate(){
-        if(rotors[0].rotate()){
-            if(rotors[1].rotate()){
+
+    private void rotate() {
+        if (rotors[0].rotate()) {
+            if (rotors[1].rotate()) {
                 rotors[2].rotate();
             }
         }
-    } 
+    }
 }
--- Rotor.java
+++ Rotor.java
@@ -2,31 +2,28 @@ public class Rotor {
 
     private String rotorValues;
     private char startChar;
+
     public Rotor(String v, char c) {
         this.rotorValues = new String(v);
         this.startChar = c;
         String copy = "";
 
-        for (int i = 0; i < rotorValues.length(); i++) {
-            if (rotorValues.charAt(i) != this.startChar) {
-                copy = copy + rotorValues.charAt(i);
-            }
+        while (rotorValues.charAt(0) != startChar) {
+            rotate();
         }
-        rotorValues = this.startChar + copy;
+
     }
 
     public boolean rotate() {
         // TODO
-        char node = rotorValues.charAt(rotorValues.length()-1);
-        if (node == startChar) {
-            String temp = rotorValues.substring(0, rotorValues.length() - 2);
-            String temp1 = rotorValues.substring(rotorValues.length() - 1, rotorValues.length() - 1);
-            rotorValues = temp1 + temp;
+        char lastChar = rotorValues.charAt(rotorValues.length() - 1);
+        if (lastChar == startChar) {
+            String firstHalf = rotorValues.substring(0, rotorValues.length() - 1);
+            rotorValues = lastChar + firstHalf;
             return true;
         }
-        String temp3 = rotorValues.substring(0, rotorValues.length() - 2);
-        String temp4 = rotorValues.substring(rotorValues.length() - 1, rotorValues.length() - 1);
-        rotorValues = temp4 + temp3;
+        String firstHalf = rotorValues.substring(0, rotorValues.length() - 1);
+        rotorValues = lastChar + firstHalf;
         return false;
     }
 
